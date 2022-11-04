9de17d30aa514347ae7185afa7fc74df32e9aef4
Helena Antoni <helenaantoni03@gmail.com>
2022-11-04 09:49:48 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+    <img src="./assets/icon-${player1}.svg " alt="Bandeira do ${player1}"/> 
+    <strong> ${hour} </strong>
+    <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+  </li>
+  `
+}
+
+let delay = -0.3;
+function createCard(date, day, games) {
+  delay = delay + 0.3;
+  return `
+    <div class="card" style="animation-delay: ${delay}s"> 
+      <h2> ${date} <span>${day}</span> </h2>  
+      <ul>
+        ${games}
+      </ul> 
+    </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "Quinta", createGame("brazil", "16:00", "serbia")) 
+  +
+  createCard("28/11","Segunda",createGame("switzerland", "13:00", "brazil") 
+  + createGame("portugal", "13:00", "uruguay")
+  ) 
+  +
+  createCard("02/12", "Sexta", createGame("brazil", "16:00", "camaroon"))
